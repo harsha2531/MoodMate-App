dd9777499c2a7bf6d358339f8e30e6e9556ad6fd
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2025-10-05 12:42:33 +0530

    add dashboard tabs

---
+++ navigation/DashboardTabs.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Timeline from '../app/(dashboard)/Timeline';
+import NewEntry from '../app/(dashboard)/NewEntry';
+import Stats from '../app/(dashboard)/Stats';
+import Profile from '../app/(dashboard)/Profile';
+
+const Tab = createBottomTabNavigator();
+
+export default function DashboardTabs() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Timeline" component={Timeline} />
+            <Tab.Screen name="New" component={NewEntry} options={{ title: 'New Entry' }} />
+            <Tab.Screen name="Stats" component={Stats} />
+            <Tab.Screen name="Profile" component={Profile} />
+        </Tab.Navigator>
+    );
+}
