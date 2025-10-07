37767d23dac308a33c233b98671b936793aae1ed
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2025-10-07 13:13:41 +0530

    add stats in protected folder

---
+++ app/(protected)/stats.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Stats() {
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>📊 Your stats will appear here</Text>
+        </View>
+    );
+}
