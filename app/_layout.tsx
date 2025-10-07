37f3ec0ae2a389ae8640d9f517e5743d02627315
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2025-10-07 21:14:44 +0530

    changed app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,15 @@
-import { Stack } from "expo-router";
-import { AuthProvider } from "../contexts/AuthContext";
+import { Stack } from 'expo-router';
+import { AuthProvider } from '../context/AuthContext';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
     return (
         <AuthProvider>
+            <StatusBar style="light" />
             <Stack screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="index" />
+                <Stack.Screen name="(tabs)" />
                 <Stack.Screen name="login" />
                 <Stack.Screen name="register" />
-                <Stack.Screen name="(protected)" />
             </Stack>
         </AuthProvider>
     );
