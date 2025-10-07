00f00b33b65e74138360d2418ca4c2fba7a34611
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2025-10-07 12:16:11 +0530

    updated App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import RootNavigator from "./navigation/RootNavigator";
 import { AuthProvider } from "./contexts/AuthContext";
+import RootNavigator from "./navigation/RootNavigator";
+import { Provider as PaperProvider } from "react-native-paper";
 
 export default function App() {
     return (
-        <AuthProvider>
-            <NavigationContainer>
-                <RootNavigator />
-            </NavigationContainer>
-        </AuthProvider>
+        <PaperProvider>
+            <AuthProvider>
+                <NavigationContainer>
+                    <RootNavigator />
+                </NavigationContainer>
+            </AuthProvider>
+        </PaperProvider>
     );
 }
