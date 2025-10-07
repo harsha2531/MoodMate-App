385130b74860b229988dfa787678efd7037bf608
Harsha Nimeda Sirithunga <harshanimeda@gmail.com>
2025-10-07 12:18:21 +0530

    created dashboard tabs

--- navigation/DashboardTabs.tsx
+++ navigation/DashboardTabs.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Timeline from '../app/(dashboard)/Timeline';
-import NewEntry from '../app/(dashboard)/NewEntry';
-import Stats from '../app/(dashboard)/Stats';
-import Profile from '../app/(dashboard)/Profile';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Timeline from "../app/(dashboard)/Timeline";
+import NewEntry from "../app/(dashboard)/NewEntry";
+import Stats from "../app/(dashboard)/Stats";
+import Profile from "../app/(dashboard)/Profile";
 
 const Tab = createBottomTabNavigator();
 
 export default function DashboardTabs() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
             <Tab.Screen name="Timeline" component={Timeline} />
-            <Tab.Screen name="New" component={NewEntry} options={{ title: 'New Entry' }} />
+            <Tab.Screen name="NewEntry" component={NewEntry} options={{ title: "New" }} />
             <Tab.Screen name="Stats" component={Stats} />
             <Tab.Screen name="Profile" component={Profile} />
         </Tab.Navigator>
